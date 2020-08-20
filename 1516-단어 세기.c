2c51ddcc5397041a6110d5e8d59b99309aaca56e
SUA-Jo <69664771+SUA-Jo@users.noreply.github.com>
2020-08-20 14:06:00 +0900

    단어세기 수정

--- 1516-단어 세기.c
+++ 1516-단어 세기.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	char array[30][200];
+	char array[30][201];
 	int i, j = 0, k[30] = { 0, };		//k는 sArr에서 한 문장의 시작 index
 	char* ptr;
 	char* sArr[100] = { NULL, };
@@ -12,6 +12,7 @@ int main(void)
 
 	for (i = 0; i < 30; i++) {
 		gets_s(array[i], 200);
+		if (strcmp(array[i], "END") == 0) break;
 		ptr = strtok(array[i], " ");
 		while (ptr != NULL)
 		{
@@ -29,44 +30,47 @@ int main(void)
 			j++;
 			ptr = strtok(NULL, " ");
 		}
-		k[i+1] = j+1;
-		if (strcmp(array[i], "END") == 0) break;
+		k[i+1] = j;
 	}
 
 	for (i = 0; i < 29; i++) {
-		for (j = k[i]; j < k[i+1]; j++) {
-			if (sArr[j] != NULL && sArr[j+1] != NULL) {
-				if (strcmp(sArr[j], sArr[j + 1])>0) {
-					char* tmp = sArr[j];
-					sArr[j] = sArr[j + 1];
-					sArr[j + 1] = tmp;
+		for (j = k[i]; j < k[i + 1]; j++) {
+			for (int z = k[i]; z < k[i + 1] - 1; z++) {
+				if (sArr[z] != NULL && sArr[z + 1] != NULL) {
+					if (strcmp(sArr[z], sArr[z + 1]) > 0) {
+						char* tmp = sArr[z];
+						sArr[z] = sArr[z + 1];
+						sArr[z + 1] = tmp;
 
-					int tmp1 = number[j];
-					number[j] = number[j + 1];
-					number[j + 1] = tmp1;
+						int tmp1 = number[z];
+						number[z] = number[z + 1];
+						number[z + 1] = tmp1;
+					}
+				}
+				else if (sArr[z + 1] == NULL && sArr[z] != NULL) {
+					int y = z + 2;
+					while (sArr[y] == NULL) {
+						y++;
+					}
+					if (y >= k[i+1]) break;
+					else if (strcmp(sArr[z], sArr[y]) > 0) {
+						char* tmp = sArr[z];
+						sArr[z] = sArr[y];
+						sArr[y] = tmp;
+
+						int tmp1 = number[z];
+						number[z] = number[y];
+						number[y] = tmp1;
+						z = y - 1;
+					}
 				}
 			}
-			else if (sArr[j + 1] == NULL && sArr[j]!=NULL) {
-				int z = j + 2;
-				while (sArr[z] == NULL) {
-					z++;
-				}
-				if (z>= 99) break;
-				else if (strcmp(sArr[j], sArr[z]) > 0) {
-					char* tmp = sArr[j];
-					sArr[j] = sArr[z];
-					sArr[z] = tmp;
-
-					int tmp1 = number[j];
-					number[j] = number[z];
-					number[z] = tmp1;
-					j = z-1;
-				}
-				
-			}
-			if (sArr[j] == NULL || strcmp(sArr[j], "END") == 0) continue;
-			printf("%s : %d\n", sArr[j], number[j]);
 		}
+	}
+
+	for (i = 0; i < 100; i++) {
+		if (sArr[i] == NULL || strcmp(sArr[i], "END") == 0) continue;
+		printf("%s : %d\n", sArr[i], number[i]);
 	}
 
 	return 0;
